d47bddf9d0afbbc6a8cf0790355976a2745be8c6
spjyotiranjan <142651812+spjyotiranjan@users.noreply.github.com>
2023-09-18 15:18:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS-Operators
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-lmanwj)
---
+++ script.js
@@ -0,0 +1,47 @@
+var x = 5;
+var y = 2;
+
+// Challenge 1: Perform the addition operation (add x and y) and print the answer in the console.
+
+// Challenge 2: Perform the Subtraction operation (subtract y from x) and print the answer in the console.
+
+// Challenge 3: Perform Multiplication operation (multiply x from y) and print the answer in console.
+
+// Challenge 4: Perform Division operation (Divide x by y) and print the answer in console.
+
+// Challenge 5: perform division operation (divide x by y) and print the remainder in console.
+
+// Challenge 6: Perform Exponent operation and print the answer in console.
+
+var z = 20;
+// Challenge 7: calculate "x" to the power of the "y" and print the answer in console.
+
+// Challenge 8: Decrement the value of the variable "z" by 1 and print the z value in the console.
+
+var a = 20;
+
+var b = 10;
+
+var c = 15;
+
+var d = 20;
+
+// Challenge 9: Perform Addition assignment operation to the variable a;
+
+// Challenge 10: Perform 	Subtraction assignment operation to the variable b;
+
+// Challenge 11: Perform 	Multiplication assignment operation to the variable c;
+
+// Challenge 12: Perform 	Division assignment operation to the variable c;
+
+// Challenge 13: Perform 2 Strict equality Comparison operations, in which one should return true and one should return false.
+
+// Challenge 14: Perform 2 Strict-non-equality Comparison operations, in which one should return true and one should return false.
+
+// Challenge 15: Perform 2 Less than Comparison operations, in which one should return true and one should return false.
+
+// Challenge 16: Perform 2 Greater than Comparison operations, in which one should return true and one should return false.
+
+// Challenge 17: Perform 2 Less than or equal to Comparison operations, in which one should return true and one should return false.
+
+// Challenge 18: Perform 2 Greater than or equal to Comparison operations, in which one should return true and one should return false.
